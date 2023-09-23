d850f31f10a9d5e1931fe3629aa32109b02eda1d
saadrafi <saadrafi81@gmail.com>
2023-09-23 08:06:13 +0600

    reset bg color of shapes added

--- JS/randomColorGenerate.js
+++ JS/randomColorGenerate.js
@@ -3,7 +3,9 @@ const Elements = document.querySelectorAll(".autoColor");
 function randomcolor() {
   return Math.floor(Math.random() * 255);
 }
-
+function resetBackgroundColor(id) {
+  document.getElementById(id).style.backgroundColor = "transparent";
+}
 function randomcolorgenerate(id) {
   const color = `rgba(${randomcolor()},${randomcolor()},${randomcolor()},${0.5})`;
   document.getElementById(id).style.backgroundColor = color;
@@ -12,5 +14,11 @@ function randomcolorgenerate(id) {
 for (Element of Elements) {
   Element.addEventListener("mouseenter", function (e) {
     randomcolorgenerate(e.target.id);
+  });
+}
+
+for (Element of Elements) {
+  Element.addEventListener("mouseleave", function (e) {
+    resetBackgroundColor(e.target.id);
   });
 }
